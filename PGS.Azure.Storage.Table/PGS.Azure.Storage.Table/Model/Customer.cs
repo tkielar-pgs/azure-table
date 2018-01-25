6f40f2b19b16574ca25465e55ec5715c94253165
Tomasz Kielar <tkielar@pgs-soft.com>
2018-01-25 11:58:05 +0100

    Sample model

---
+++ PGS.Azure.Storage.Table/PGS.Azure.Storage.Table/Model/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PGS.Azure.Storage.Table.Model
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNo { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double Discount { get; set; }
+    }
+}
---
+++ PGS.Azure.Storage.Table/PGS.Azure.Storage.Table/Model/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PGS.Azure.Storage.Table.Model
+{
+    public class Order
+    {
+        public DateTime OrderedTime { get; set; }
+        public double TotalAmount { get; set; }
+        public bool IsShipped { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
