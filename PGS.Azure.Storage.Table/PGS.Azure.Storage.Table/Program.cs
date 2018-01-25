6868a48a006533f6c10c150f42b454f94ff7dcb4
Tomasz Kielar <tkielar@pgs-soft.com>
2018-01-25 13:58:56 +0100

    Getting table reference

--- PGS.Azure.Storage.Table/PGS.Azure.Storage.Table/Program.cs
+++ PGS.Azure.Storage.Table/PGS.Azure.Storage.Table/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System.IO;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
 using PGS.Azure.Storage.Table.Configuration;
 
 namespace PGS.Azure.Storage.Table
@@ -9,6 +13,17 @@ namespace PGS.Azure.Storage.Table
         static void Main(string[] args)
         {
             StorageAccountOptions storageAccountOptions = ParseStorageAccountOptions();
+
+            CloudTable tableReference = GetTableReferenceAsync(storageAccountOptions).GetAwaiter().GetResult();
+        }
+
+        private static async Task<CloudTable> GetTableReferenceAsync(StorageAccountOptions storageAccountOptions)
+        {
+            var storageAccount = new CloudStorageAccount(new StorageCredentials(storageAccountOptions.Name, storageAccountOptions.Key), true);
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable tableReference = tableClient.GetTableReference(storageAccountOptions.TableName);
+            await tableReference.CreateIfNotExistsAsync();
+            return tableReference;
         }
 
         private static StorageAccountOptions ParseStorageAccountOptions()
