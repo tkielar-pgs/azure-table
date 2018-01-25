f19c77333d631867a719aa02d0cd131c029fa069
Tomasz Kielar <tkielar@pgs-soft.com>
2018-01-25 16:24:15 +0100

    More data samples

--- PGS.Azure.Storage.Table/PGS.Azure.Storage.Table/DataSamples/CustomerSamples.cs
+++ PGS.Azure.Storage.Table/PGS.Azure.Storage.Table/DataSamples/CustomerSamples.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using PGS.Azure.Storage.Table.Model;
 
 namespace PGS.Azure.Storage.Table.DataSamples
@@ -13,5 +14,33 @@ namespace PGS.Azure.Storage.Table.DataSamples
             LastName = "Duck",
             PhoneNo = "555-113-45"
         };
+
+        public static Customer BugsBunny => new Customer
+        {
+            BirthDate = new DateTime(1940, 7, 27),
+            Discount = 17,
+            FirstName = "Bugs",
+            LastName = "Bunny",
+            PhoneNo = "555-1313-135"
+        };
+
+        public static Customer DaffyDuck => new Customer
+        {
+            BirthDate = new DateTime(1937, 4, 17),
+            Discount = 7.25,
+            FirstName = "Daffy",
+            LastName = "Duck",
+            PhoneNo = "555-871-5212"
+        };
+
+        public static IEnumerable<Customer> AllCustomers
+        {
+            get
+            {
+                yield return DonaldDuck;
+                yield return BugsBunny;
+                yield return DaffyDuck;
+            }
+        }
     }
 }
--- PGS.Azure.Storage.Table/PGS.Azure.Storage.Table/DataSamples/OrderSamples.cs
+++ PGS.Azure.Storage.Table/PGS.Azure.Storage.Table/DataSamples/OrderSamples.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using PGS.Azure.Storage.Table.Model;
 
 namespace PGS.Azure.Storage.Table.DataSamples
@@ -13,5 +14,33 @@ namespace PGS.Azure.Storage.Table.DataSamples
             OrderedTime = new DateTime(2018, 1, 25, 14, 41, 43),
             TotalAmount = 110.24
         };
+
+        public static Order DonaldsOrder2 => new Order
+        {
+            Id = Guid.Parse("ca15aaa1-d5e8-4532-ac84-cfea6f1631e5"),
+            CustomerKey = CustomerSamples.DonaldDuck.RowKey,
+            IsShipped = true,
+            OrderedTime = new DateTime(2018, 1, 25, 16, 15, 27),
+            TotalAmount = 431.19
+        };
+
+        public static Order DaffysOrder => new Order
+        {
+            Id = Guid.Parse("52d95705-7f96-45eb-a25a-4776ccaa1634"),
+            CustomerKey = CustomerSamples.DaffyDuck.RowKey,
+            IsShipped = false,
+            OrderedTime = new DateTime(2017, 12, 24, 4, 5, 12),
+            TotalAmount = 12.2
+        };
+
+        public static IEnumerable<Order> AllOrders
+        {
+            get
+            {
+                yield return DonaldsOrder;
+                yield return DonaldsOrder2;
+                yield return DaffysOrder;
+            }
+        }
     }
 }
