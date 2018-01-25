da21517aefefec5492c2d7af2420f2b62daf1ab2
Tomasz Kielar <tkielar@pgs-soft.com>
2018-01-25 16:14:13 +0100

    Fixed Order -> Customer reference

--- PGS.Azure.Storage.Table/PGS.Azure.Storage.Table/DataSamples/OrderSamples.cs
+++ PGS.Azure.Storage.Table/PGS.Azure.Storage.Table/DataSamples/OrderSamples.cs
@@ -8,7 +8,7 @@ namespace PGS.Azure.Storage.Table.DataSamples
         public static Order DonaldsOrder => new Order
         {
             Id = Guid.Parse("6a456c57-ec65-4ea5-a167-e95d70c57016"),
-            Customer = CustomerSamples.DonaldDuck,
+            CustomerKey = CustomerSamples.DonaldDuck.RowKey,
             IsShipped = false,
             OrderedTime = new DateTime(2018, 1, 25, 14, 41, 43),
             TotalAmount = 110.24
--- PGS.Azure.Storage.Table/PGS.Azure.Storage.Table/Model/Order.cs
+++ PGS.Azure.Storage.Table/PGS.Azure.Storage.Table/Model/Order.cs
@@ -17,6 +17,6 @@ namespace PGS.Azure.Storage.Table.Model
         public DateTime OrderedTime { get; set; }
         public double TotalAmount { get; set; }
         public bool IsShipped { get; set; }
-        public Customer Customer { get; set; }
+        public string CustomerKey { get; set; }
     }
 }
